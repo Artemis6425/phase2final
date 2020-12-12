18b9bc6a0f624841dc7b02de67dac65186a636c7
Artemis6425 <aurorarose6425@gmail.com>
2020-12-12 13:00:42 -0600

    controllers getting paths and general info

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,3 +1,27 @@
 class RunsController < ApplicationController
 
+    get '/runs' do
+        @runs = Run.all
+        erb :'run/index'
+    end
+
+    get '/runs/new' do
+        erb :'run/new'
+    end
+
+    get '/runs/:id' do
+        @run = Run.find(params[:id])
+        @user = User.find_by_id(@run.user_id)
+        erb :'run/show'
+    end
+
+    get '/runs/:id/edit' do
+        @run = Run.find(params[:id])
+        if @run.user_id == session.id           #something like that
+            erb :'run/edit'
+        else
+            redirect '/runs'
+        end
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,19 @@
 class UsersController < ApplicationController
 
+    get '/users' do
+        @users = User.all
+        erb :'/users/index'
+    end
+
+    get '/users/new' do
+        erb :'/users/new'
+    end
+
+    get '/users/:id' do
+        @user = User.find(params[:id])
+        @user_runs = Run.find_by_user_id(@user.id)
+        erb :'/owners/show'
+    end
+
+
 end
